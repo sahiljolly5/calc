4fbd9fe88d9f34db9c672799516999d9686fb261
sahil jolly <sahiljolly5@gmail.com>
2021-08-30 12:23:37 +0530

    ID Card

--- submit.js
+++ submit.js
@@ -2,33 +2,50 @@
 function download()
 
 {
+    var inputA = document.getElementById("name").value
+    var inputB = document.getElementById('Age').value
+    var inputC = document.getElementById('mail').value
+    var inputD = document.getElementById('phone').value
+    var inputZ = document.getElementById('addr').value
 
     //if (document.getElementById('name').value ="    "){
 //alert("enter name");
+if(inputA == '')
+{
+    alert('Enter Name')
+}
+else if(inputB == '')
+{
+    alert('Enter Age')
+}
+else if(inputC == '')
+{
+    alert('Enter Email')
+}
+else if(inputD == '')
+{
+    alert('Enter Phone')
+}
+else if(inputZ == '')
+{
+    alert('Enter Address')
+}
 
     
      
-  
-   
+else {
+
+    document.getElementById('demo2').innerHTML = inputA 
+
+    document.getElementById('demo3').innerHTML = inputB 
     
-    //document.getElementsByClassName("output").style.display = "block";
-
-
-var inputA = document.getElementById("name").value
-document.getElementById('demo2').innerHTML = inputA 
-
-var inputB = document.getElementById('Age').value
-document.getElementById('demo3').innerHTML = inputB 
-
-
-var inputC = document.getElementById('mail').value
-document.getElementById('demo4').innerHTML = inputC
-
-var inputD = document.getElementById('phone').value
-document.getElementById('demo5').innerHTML = inputD
-
-var inputZ = document.getElementById('addr').value
-document.getElementById('demo6').innerHTML = inputZ
-
-
+    
+    document.getElementById('demo4').innerHTML = inputC
+    
+    document.getElementById('demo5').innerHTML = inputD
+    
+    document.getElementById('demo6').innerHTML = inputZ
+    
+    document.getElementById('out').style.display = 'block'
+}
 }
